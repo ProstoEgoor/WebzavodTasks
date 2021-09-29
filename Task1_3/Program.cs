08ef354071643a13f848ecba56deef0856fb8f75
ProstoEgor <prosto.egor2108@gmail.com>
2021-09-29 06:55:42 +0400

    task 1.3

---
+++ Task1_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            int length = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            long sum = 0;
+
+            Console.WriteLine("Введите числа:");
+
+            while(tryReadInt(out number))
+            {
+                ++length;
+                if (number < min)
+                    min = number;
+                if (number > max)
+                    max = number;
+                sum += number;
+            }
+
+            double mean = sum / length;
+
+            Console.WriteLine("Количество чисел:\t" + length);
+            Console.WriteLine("Минимальное число:\t" + min);
+            Console.WriteLine("Максимальное число:\t" + max);
+            Console.WriteLine("Сумма чисел:\t\t" + sum);
+            Console.WriteLine("Среднее чисел:\t\t" + mean);
+        }
+
+        static bool tryReadInt(out int number)
+        {
+            string line;
+            while(true)
+            {
+                line = Console.ReadLine();
+                if(line.Length == 0)
+                {
+                    number = 0;
+                    return false;
+                } else if(int.TryParse(line, out number) && -1000 <= number && number <= 1000)
+                {
+                    return true;
+                } else
+                {
+                    Console.WriteLine("Неверный ввод! Если хотите закончить ввод введите пустую строку.");
+                }
+            }
+            
+        }
+    }
+}
