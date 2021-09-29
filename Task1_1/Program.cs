228fd8ac78f7f8ebb660eb07dfa1781f695a1b27
ProstoEgor <prosto.egor2108@gmail.com>
2021-09-29 06:04:45 +0400

    task 1.1

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -6,7 +6,21 @@ namespace Task1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите число: ");
+            long number;
+            while(!long.TryParse(Console.ReadLine(), out number) || number < -1_000_000 || number > 1_000_000)
+            {
+                Console.WriteLine("Неверный ввод!");
+                Console.Write("Введите число: ");
+            }
+
+            Console.WriteLine(number + " в квадрате равно: " + getSquare(number));
         }
+
+        static long getSquare(long number)
+        {
+            return number * number;
+        }
+
     }
 }
