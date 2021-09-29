857decb2707eb731d24eb6fcbcc4f201830d1ffe
ProstoEgor <prosto.egor2108@gmail.com>
2021-09-29 06:30:53 +0400

    task 1.2

--- Task1_2/Program.cs
+++ Task1_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Task1_2
 {
@@ -6,7 +7,30 @@ namespace Task1_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите значение: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Тип введенного значения: " + getType(text));
+        }
+
+        static string getType(string value)
+        {
+            NumberStyles style = NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+
+            if (long.TryParse(value, out _))
+            {
+                return "целое число";
+            } 
+            if (double.TryParse(value, style, culture, out _))
+            {
+                return "дробное число";
+            } 
+            if (bool.TryParse(value, out _))
+            {
+                return "логическое значение";
+            }
+            return "текст";
         }
     }
 }
