302d537324761e21d391c5cdc0264500fdaa3257
ProstoEgor <prosto.egor2108@gmail.com>
2021-10-06 16:32:31 +0400

    Add task 2.1.1

--- Topic_2/Task1_1/Program.cs
+++ Topic_2/Task1_1/Program.cs
@@ -3,7 +3,20 @@
 namespace Task1_1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo pressedKey;
+            while (true) {
+                pressedKey = Console.ReadKey(true);
+                if(pressedKey.KeyChar == 0 
+                    || pressedKey.Key == ConsoleKey.Spacebar
+                    || pressedKey.Key == ConsoleKey.Enter
+                    || pressedKey.Key == ConsoleKey.Backspace
+                    || pressedKey.Key == ConsoleKey.Escape
+                    || pressedKey.Key == ConsoleKey.Tab) {
+                    Console.WriteLine($"Введенная клавиша: {pressedKey.Key}, код клавиши отсутствует.");
+                } else {
+                    Console.WriteLine($"Введенная клавиша: {pressedKey.KeyChar}, код клавиши: 0x{(int)pressedKey.KeyChar:x4}");
+                }
+            }
         }
     }
 }
