00a7ad431b13e287d67671286b540bf8f19dee2f
ProstoEgor <prosto.egor2108@gmail.com>
2021-10-07 10:52:49 +0400

    add task 2.1.3

---
+++ Topic_2/Task1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1_3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Введите слова:");
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] sortedWords = words.Select(word => word.ToUpper()).OrderBy(word => word).ToArray();
+            foreach (var word in sortedWords) {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
