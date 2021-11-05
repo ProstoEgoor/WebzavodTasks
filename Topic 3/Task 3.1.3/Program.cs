5c0424c4f789fd92db1b37573b48e513d36acea0
ProstoEgor <prosto.egor2108@gmail.com>
2021-11-06 02:56:59 +0400

    Add error highlight for task 3.1.3

--- Topic 3/Task 3.1.3/Program.cs
+++ Topic 3/Task 3.1.3/Program.cs
@@ -6,10 +6,22 @@ namespace Task_3._1._3 {
         static void Main(string[] args) {
             Console.WriteLine("Введите скобочную последовательность для проверки:");
             string line = Console.ReadLine();
-            Console.WriteLine(CheckBracketSequence(line));
+            bool[] errorPositions = new bool[line.Length];
+            if (CheckBracketSequence(line, errorPositions)) {
+                Console.WriteLine("Последовательность правильная.");
+            } else {
+                Console.WriteLine("Последовательность не правильная.");
+                for (int i = 0; i < line.Length; i++) {
+                    if (errorPositions[i]) {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write(line[i]);
+                    Console.ResetColor();
+                }
+            }
         }
 
-        static bool CheckBracketSequence(string line) {
+        static bool CheckBracketSequence(string line, bool[] errorPositions) {
             Dictionary<char, char> bracketPairs = new Dictionary<char, char> {
                 ['('] = ')',
                 ['{'] = '}',
@@ -23,13 +35,19 @@ namespace Task_3._1._3 {
                 if (bracketPairs.ContainsKey(line[i])) {
                     bracketStack.Push(line[i]);
                 } else if (bracketPairs.ContainsValue(line[i])) {
-                    if  (bracketStack.Count == 0 || !(bracketPairs[bracketStack.Pop()] == line[i])) {
+                    if (bracketStack.Count == 0 || !(bracketPairs[bracketStack.Pop()] == line[i])) {
+                        errorPositions[i] = true;
                         return false;
                     }
                 }
             }
 
             if (bracketStack.Count > 0) {
+                int pos = line.Length;
+                while(bracketStack.Count > 0) {
+                    pos = line.LastIndexOf(bracketStack.Pop(), pos - 1);
+                    errorPositions[pos] = true;
+                }
                 return false;
             }
 
