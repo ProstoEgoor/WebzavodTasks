e73bc9a820ed679e4654aa9fe41c544519a81199
ProstoEgor <prosto.egor2108@gmail.com>
2021-11-05 12:53:24 +0400

    add Task 3.1.3 simple variant

---
+++ test/B.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test {
+    class B : A {
+        public override void Foo() {
+            Console.WriteLine("B");
+        }
+    }
+}
