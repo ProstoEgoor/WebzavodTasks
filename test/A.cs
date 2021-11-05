1f079a2266af572fb62dd13b83c4f7fa4fef896d
ProstoEgor <prosto.egor2108@gmail.com>
2021-11-06 02:26:40 +0400

    Revert "Revert "add Task 3.1.3 simple variant""
    This reverts commit fc1ab8de9804bba40658ce99560c7f55e1f182c0.

---
+++ test/A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test {
+    class A {
+        public virtual void Foo() {
+            Console.WriteLine("A");
+        }
+    }
+}
