450b25dd1bc9feff88a8649e455eec0effa46045
ProstoEgor <prosto.egor2108@gmail.com>
2021-11-08 02:16:37 +0400

    Add task 6.1.2

---
+++ Topic 6/Task 6.1.2/AlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_6._1._2 {
+    public class AlreadyExistsException : Exception {
+        public string Value { get; } 
+        public int Position { get; }
+
+        public AlreadyExistsException(string value, int position) {
+            Value = value;
+            Position = position;
+        }
+    }
+}
---
+++ Topic 6/Task 6.1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_6._1._2 {
+    class Program {
+        static void Main(string[] args) {
+            Dictionary<string, int> enteredValues = new Dictionary<string, int>();
+            int position = 1;
+            try {
+                while (true) {
+                    Console.Write($"Введите значение №{position}: ");
+                    string value = Console.ReadLine().Trim().ToLower();
+                    if (enteredValues.ContainsKey(value)) {
+                        throw new AlreadyExistsException(value, enteredValues[value]);
+                    } else {
+                        enteredValues[value] = position++;
+                    }
+                }
+            } catch (AlreadyExistsException e) {
+                Console.WriteLine($"Значение \"{e.Value}\" уже было введенно раннее на позиции №{e.Position}.");
+            }
+        }
+    }
+}
