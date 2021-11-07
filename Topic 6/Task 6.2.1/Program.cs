90733701f21903d0af8411418c5e646a74675149
ProstoEgor <prosto.egor2108@gmail.com>
2021-11-08 03:14:06 +0400

    Add task 6.2.1

---
+++ Topic 6/Task 6.2.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_6._2._1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Введите объем цистерны:");
+            Tank tank = new Tank(ReadVolume());
+            Console.WriteLine("Для добавления жидкости введите строку в формате \"+ жидкость_в_литрах\"");
+            Console.WriteLine("Для убавления жидкости введите строку в формате \"- жидкость_в_литрах\"");
+            Regex actionRegex = new Regex(@"([+-])\s*([1-9]\d*)");
+            while (true) {
+                try {
+                    string actionLine = Console.ReadLine();
+                    Match actionMatch = actionRegex.Match(actionLine);
+                    if (actionMatch.Success) {
+                        if (actionMatch.Groups[1].Value == "+") {
+                            tank.Add(uint.Parse(actionMatch.Groups[2].Value));
+                        } else {
+                            tank.Take(uint.Parse(actionMatch.Groups[2].Value));
+                        }
+                        Console.WriteLine(tank.ToString());
+                    } else {
+                        Console.WriteLine("Неверный ввод. Повторите ввод.");
+                    }
+                } catch (TankOverflowException e) {
+                    Console.WriteLine(e.Message);
+                } catch (NotEnoughException e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static uint ReadVolume() {
+            uint answer;
+            while (!uint.TryParse(Console.ReadLine(), out answer) || answer == 0) {
+                Console.WriteLine("Неверный ввод. Повторите ввод.");
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Topic 6/Task 6.2.1/Tank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_6._2._1 {
+    class Tank {
+        public uint MaxVolume { get; }
+
+        int currentVolume;
+        public int CurrentVolume {
+            get => currentVolume;
+            set {
+                if (value > MaxVolume) {
+                    throw new TankOverflowException();
+                }
+
+                if (value < 0) {
+                    throw new NotEnoughException();
+                }
+
+                currentVolume = value;
+            }
+        }
+
+        public Tank(uint maxVolume) {
+            if (maxVolume == 0) {
+                throw new ArgumentOutOfRangeException("Объем цистерны должен быть больше нуля.");
+            }
+            MaxVolume = maxVolume;
+        }
+
+        public void Add(uint liquidVolume) {
+            CurrentVolume += (int)liquidVolume;
+        }
+
+        public void Take(uint liquidVolume) {
+            CurrentVolume -= (int)liquidVolume;
+        }
+
+        public override string ToString() {
+            return $"Цистерна заполнена на {CurrentVolume} л из {MaxVolume} л.";
+        }
+    }
+
+    class TankOverflowException : Exception {
+        public TankOverflowException() : base("В цистерне не хватает места.") { }
+    }
+    class NotEnoughException : Exception {
+        public NotEnoughException() : base("В цистерне недостаточно жидкости.") { }
+    }
+}
