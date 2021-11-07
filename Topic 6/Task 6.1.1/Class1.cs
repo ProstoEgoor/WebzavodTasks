a83d15496af8635ac18a23753dea016b77864220
ProstoEgor <prosto.egor2108@gmail.com>
2021-11-08 01:58:49 +0400

    Add task 6.1.1

---
+++ Topic 6/Task 6.1.1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_6._1._1 {
+    class Class1 {
+        public delegate Class1 GetNewClass1();
+        public delegate Class1 TransformClass1(Class1 obj);
+        public delegate string GetClass1Description(Class1 obj, string comment);
+
+        GetNewClass1 Generator;
+        TransformClass1 Transformer;
+
+        public Class1(GetClass1Description d1, GetClass1Description d2) {
+            OnDescribe += d1;
+            OnDescribe += d2;
+        }
+
+        public event GetClass1Description OnDescribe;
+    }
+}
