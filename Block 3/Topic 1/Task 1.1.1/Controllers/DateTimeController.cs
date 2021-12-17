3e47d23eb9d7c3cf845634ba0c54e2fd49e81b4c
ProstoEgor <prosto.egor2108@gmail.com>
2021-12-18 00:17:42 +0400

    Added task 1.1.1

---
+++ Block 3/Topic 1/Task 1.1.1/Controllers/DateTimeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_1._1._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateTimeController : ControllerBase
+    {
+        public DateTime CurrentTime = DateTime.Now;
+
+        [HttpGet("/date")]
+        public IActionResult GetDate([FromQuery] string format = "dd.MM.yyyy")
+        {
+            return Ok(CurrentTime.ToString(format));
+        }
+
+        [HttpGet("/time")]
+        public IActionResult GetTime([FromQuery] string format = "HH:mm:ss")
+        {
+            return Ok(CurrentTime.ToString(format));
+        }
+    }
+}
