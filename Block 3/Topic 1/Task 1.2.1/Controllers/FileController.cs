3dd810fb3d36834d68a481ed8f3708de8bfafcab
ProstoEgor <prosto.egor2108@gmail.com>
2021-12-18 13:40:55 +0400

    Added task 1.2.1

---
+++ Block 3/Topic 1/Task 1.2.1/Controllers/FileController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task_1._2._1.Services;
+
+namespace Task_1._2._1.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase {
+        public IFileService FileService { get; }
+        public FileController(IFileService fileService) {
+            FileService = fileService;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get([FromQuery] int startRow, [FromQuery] int endRow) {
+            string text;
+            Exception e;
+            if (startRow == 0 && endRow == 0) {
+                (text, e) = await FileService.GetAsync();
+            } else {
+                (text, e) = await FileService.GetAsync(startRow, endRow);
+            }
+
+            if (text != null) {
+                return Ok(text);
+            } else {
+                if (e is ArgumentOutOfRangeException) {
+                    return NotFound(e.Message);
+                } else {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+
+        [HttpGet("{row}")]
+        public async Task<IActionResult> Get([FromRoute] int row) {
+            (string text, Exception e) = await FileService.GetAsync(row);
+            if (text != null) {
+                return Ok(text);
+            } else {
+                if (e is ArgumentOutOfRangeException) {
+                    return NotFound(e.Message);
+                } else {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody] string text, [FromQuery] bool force) {
+            (bool success, Exception e) = await FileService.PutAsync(text, force);
+            if (success) {
+                return Ok();
+            } else {
+                if (e is ArgumentException) {
+                    return StatusCode(StatusCodes.Status422UnprocessableEntity, e.Message);
+                } else {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+
+        [HttpPut("{row}")]
+        public async Task<IActionResult> Put([FromRoute] int row, [FromBody] string text) {
+            (bool success, Exception e) = await FileService.PutAsync(text, row);
+            if (success) {
+                return Ok();
+            } else {
+                if (e is ArgumentOutOfRangeException) {
+                    return NotFound(e.Message);
+                } else {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+
+        [HttpDelete("{row}")]
+        public async Task<IActionResult> Delete([FromRoute] int row) {
+            (bool success, Exception e) = await FileService.DeleteAsync(row);
+            if (success) {
+                return Ok();
+            } else {
+                if (e is ArgumentOutOfRangeException) {
+                    return NotFound(e.Message);
+                } else {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Block 3/Topic 1/Task 1.2.1/Services/FileService.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_1._2._1.Services {
+    public interface IFileService {
+        Task<(string, Exception)> GetAsync();
+        Task<(string, Exception)> GetAsync(int row);
+        Task<(string, Exception)> GetAsync(int startRow, int endRow);
+
+        Task<(bool, Exception)> PutAsync(string text, bool force);
+        Task<(bool, Exception)> PutAsync(string text, int row);
+
+        Task<(bool, Exception)> DeleteAsync(int row);
+    }
+
+    public class FileService : IFileService {
+        public string FilePath { get; }
+
+        public FileService(IConfiguration configuration) {
+            FilePath = Path.Combine(Directory.GetCurrentDirectory(), configuration["FileName"] ?? "text.txt");
+            if (!File.Exists(FilePath)) {
+                File.Create(FilePath).Close();
+            }
+        }
+
+        public async Task<(string, Exception)> GetAsync() {
+            try {
+                return (await File.ReadAllTextAsync(FilePath), null);
+            } catch (Exception e) {
+                return (null, e);
+            }
+        }
+
+        public async Task<(string, Exception)> GetAsync(int row) {
+            if (row < 1) {
+                return (null, new ArgumentOutOfRangeException(null, "В файле нет строки с таким номером."));
+            }
+
+            try {
+                using var stream = File.OpenText(FilePath);
+                for (int i = 0; i < row - 1 && !stream.EndOfStream; i++) {
+                    await stream.ReadLineAsync();
+                }
+
+                if (stream.EndOfStream) {
+                    return (null, new ArgumentOutOfRangeException(null, "В файле нет строки с таким номером."));
+                } else {
+                    return (await stream.ReadLineAsync(), null);
+                }
+            } catch (Exception e) {
+                return (null, e);
+            }
+        }
+
+        public async Task<(string, Exception)> GetAsync(int startRow, int endRow) {
+            if (startRow < 1 || endRow <= startRow) {
+                return (null, new ArgumentOutOfRangeException(null, "В файле нет строк с такими номерами."));
+            }
+
+            try {
+                using var stream = File.OpenText(FilePath);
+                for (int i = 0; i < startRow - 1 && !stream.EndOfStream; i++) {
+                    await stream.ReadLineAsync();
+                }
+
+                if (stream.EndOfStream) {
+                    return (null, new ArgumentOutOfRangeException(null, "В файле нет строк с такими номерами."));
+                }
+
+                var sb = new StringBuilder();
+
+                int j;
+                for (j = startRow; j < endRow && !stream.EndOfStream; j++) {
+                    sb.Append(await stream.ReadLineAsync());
+                    sb.Append("\n");
+                }
+
+                if (j != endRow) {
+                    return (null, new ArgumentOutOfRangeException(null, "В файле нет строк с такими номерами."));
+                } else {
+                    return (sb.ToString(), null);
+                }
+            } catch (Exception e) {
+                return (null, e);
+            }
+        }
+
+        public async Task<(bool, Exception)> PutAsync(string text, bool force) {
+            try {
+                if (!force) {
+                    int lineIndex = await IndexOfLine(text);
+                    if (lineIndex != -1) {
+                        return (false, new ArgumentException($"Данный текст уже сохранен в строке {lineIndex + 1}."));
+                    }
+                }
+
+                text += "\r\n";
+                await File.AppendAllTextAsync(FilePath, text);
+                return (true, null);
+            } catch (Exception e) {
+                return (false, e);
+            }
+        }
+
+        public async Task<int> IndexOfLine(string text) {
+            return (await File.ReadAllLinesAsync(FilePath)).ToList().FindIndex(line => line == text);
+        }
+
+        public async Task<(bool, Exception)> PutAsync(string text, int row) {
+            if (row < 1) {
+                return (false, new ArgumentOutOfRangeException(null, "В файле нет строки с таким номером."));
+            }
+
+            try {
+                var lines = await File.ReadAllLinesAsync(FilePath);
+
+                if (lines.Length < row) {
+                    return (false, new ArgumentOutOfRangeException(null, "В файле нет строки с таким номером."));
+                }
+
+                if (lines[row - 1] != text) {
+                    lines[row - 1] = text;
+                    await File.WriteAllLinesAsync(FilePath, lines);
+                }
+
+                return (true, null);
+            } catch (Exception e) {
+                return (false, e);
+            }
+        }
+
+        public async Task<(bool, Exception)> DeleteAsync(int row) {
+            if (row < 1) {
+                return (false, new ArgumentOutOfRangeException(null, "В файле нет строки с таким номером."));
+            }
+
+            try {
+                var lines = await File.ReadAllLinesAsync(FilePath);
+
+                if (lines.Length < row) {
+                    return (false, new ArgumentOutOfRangeException(null, "В файле нет строки с таким номером."));
+                }
+
+                await File.WriteAllLinesAsync(FilePath, lines.Select((line, index) => (line, index)).Where(item => item.index != row - 1).Select(item => item.line));
+
+                return (true, null);
+            } catch (Exception e) {
+                return (false, e);
+            }
+        }
+    }
+}
